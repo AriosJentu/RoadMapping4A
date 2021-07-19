16f6e23898b27e178fb20d6745d386d743497183
AriosJentu <darthpoezd@gmail.com>
2021-07-19 18:30:58 +1000

    Add testing function for generating map from parameters

--- test.py
+++ test.py
@@ -1,56 +1,108 @@
 import Abstracts
 import BasicElements
 import MapElements
+import MapGenerator
+import Visualizer
 
-OutsideLines = Abstracts.AbstractMapLineParameters(thickness=3, length=10)
-InsideLines = Abstracts.AbstractMapLineParameters(thickness=2, length=5)
-ConnectingLines = Abstracts.AbstractMapLineParameters(thickness=1, length=2)
+def test_1():
 
-OutsideCircles = Abstracts.AbstractMapCircleParameters(radius=4)
-InsideCircles = Abstracts.AbstractMapCircleParameters(radius=5)
+	OutsideLines = Abstracts.AbstractMapLineParameters(thickness=3, length=10)
+	InsideLines = Abstracts.AbstractMapLineParameters(thickness=2, length=5)
+	ConnectingLines = Abstracts.AbstractMapLineParameters(thickness=1, length=2)
 
-LinesInfo = Abstracts.AbstractMapLines(outside=OutsideLines, inside=InsideLines, connecting=ConnectingLines)
-CirclesInfo = Abstracts.AbstractMapCircles(outside=OutsideCircles, inside=InsideCircles)
+	OutsideCircles = Abstracts.AbstractMapCircleParameters(radius=4)
+	InsideCircles = Abstracts.AbstractMapCircleParameters(radius=5)
 
-OutsideLine = MapElements.MapLine(
-	BasicElements.Point(0, 0), 
-	BasicElements.Point(1, 1),
-	OutsideLines
-)
+	LinesInfo = Abstracts.AbstractMapLines(outside=OutsideLines, inside=InsideLines, connecting=ConnectingLines)
+	CirclesInfo = Abstracts.AbstractMapCircles(outside=OutsideCircles, inside=InsideCircles)
 
-InsideLine = MapElements.MapLine(
-	BasicElements.Point(1, 0), 
-	BasicElements.Point(2, 1),
-	InsideLines
-)
+	OutsideLine = MapElements.MapLine(
+		BasicElements.Point(0, 0), 
+		BasicElements.Point(1, 1),
+		OutsideLines
+	)
 
-ConnectingLine = MapElements.MapLine(
-	BasicElements.Point(2, 0), 
-	BasicElements.Point(3, 1),
-	ConnectingLines
-)
+	InsideLine = MapElements.MapLine(
+		BasicElements.Point(1, 0), 
+		BasicElements.Point(2, 1),
+		InsideLines
+	)
 
-OutsideCircle = MapElements.MapCircle(
-	BasicElements.Point(0, 5),
-	OutsideCircles
-)
+	ConnectingLine = MapElements.MapLine(
+		BasicElements.Point(2, 0), 
+		BasicElements.Point(3, 1),
+		ConnectingLines
+	)
 
-InsideCircle = MapElements.MapCircle(
-	BasicElements.Point(5, 5),
-	InsideCircles
-)
+	OutsideCircle = MapElements.MapCircle(
+		BasicElements.Point(0, 5),
+		OutsideCircles
+	)
 
-print(LinesInfo)
-print(CirclesInfo)
+	InsideCircle = MapElements.MapCircle(
+		BasicElements.Point(5, 5),
+		InsideCircles
+	)
 
-print(OutsideLine)
-print(InsideLine)
-print(ConnectingLine)
-print(OutsideCircle)
-print(InsideCircle)
+	print(LinesInfo)
+	print(CirclesInfo)
 
-import matplotlib.pyplot as plt
+	print(OutsideLine)
+	print(InsideLine)
+	print(ConnectingLine)
+	print(OutsideCircle)
+	print(InsideCircle)
 
-a, b = OutsideLine.get_xy_lists()
-plt.plot(a, b)
-plt.show()
+def test_2():
+	
+	outside_line_thickness = 3 
+	outside_line_length = 100
+	
+	inside_line_thickness = 2
+	inside_line_length = 60
+	inside_line_distance_from_previous = 10
+	
+	central_line_thickness = 2
+	
+	connecting_line_thickness = 2
+	connecting_line_separation_distance = 15
+
+	outside_circle_radius = 5
+	inside_circle_radius = 6
+	connecting_circle_radius = 2
+	
+	rings_radius = 15
+	rings_thickness = 2
+
+	sides_count = 3
+	rings_count = 1
+	generation_count = 1
+
+	outside_line = Abstracts.AbstractMapLineParameters(outside_line_thickness, outside_line_length)
+	inside_line = Abstracts.AbstractMapLineParameters(inside_line_thickness, inside_line_length, inside_line_distance_from_previous)
+	central_line = Abstracts.AbstractMapLineParameters(central_line_thickness)
+	connecting_line = Abstracts.AbstractMapLineParameters(connecting_line_thickness, 0, connecting_line_separation_distance)
+
+	outside_circle = Abstracts.AbstractMapCircleParameters(outside_circle_radius)
+	inside_circle = Abstracts.AbstractMapCircleParameters(inside_circle_radius)
+	connecting_circle = Abstracts.AbstractMapCircleParameters(connecting_circle_radius)
+
+	inside_rings = Abstracts.AbstractMapCircleParameters(rings_radius, rings_thickness)
+
+	generated_map = MapGenerator.Generator(
+		outside_line=outside_line,
+		inside_line=inside_line,
+		central_line=central_line,
+		connecting_line=connecting_line,
+		outside_circle=outside_circle,
+		inside_circle=inside_circle,
+		inside_rings=inside_rings,
+		connecting_circle=connecting_circle,
+		sides_count=sides_count,
+		rings_count=rings_count,
+		generation_count=generation_count
+	).generate()
+	visualizer = Visualizer.Visualizer(generated_map)
+	visualizer.save_image("Hello world.png")
+
+test_2()
